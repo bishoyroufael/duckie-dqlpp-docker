dfc4270f668c2374c47a92ee03ebaed16a27b53e
Bishoy Roufael <babdelshah@jacobs-university.de>
2022-05-01 21:24:44 +0200

    removed syntax error

--- api.py
+++ api.py
@@ -13,5 +13,3 @@ def main():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-~                                                                                                                                                                   
-~                             
