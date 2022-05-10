24c2f3309d71656be6859ceab14511915b8795f0
bishoyroufael <bishoyroufael@gmail.com>
2022-05-09 19:51:20 -0500

    updated api endpoints

--- src/api.py
+++ src/api.py
@@ -4,11 +4,22 @@ from duckieenv import DuckieEnv
 
 real_env  = DuckieEnv()
 
-@app.route('/step', methods=['GET'])
+@app.route('/step', methods=['GET', 'POST'])
 def step():
-    depth_array, reward, terminate, info = real_env.step()
+    d = request.args.get("action")
+    if not d:
+        return make_response(jsonify({"err": "action is invalid, Got None!"}),404)
+    action = int(d)
+    depth_array, reward, terminate, info = real_env.step(action)
+    info["tof_range"] = info["tof_range"].tolist()
     data = {'depth_array': depth_array.tolist(), 'reward': reward, 'terminate': terminate, 'info': info}
-    return  make_response(jsonify(data), 200)
+    jsonified = None
+    try:
+        jsonified = jsonify(data) 
+    except:
+        real_env.stop_bot()
+    
+    return  make_response(jsonified, 200)
 
 
 @app.route('/reset', methods=['GET'])
